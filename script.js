87f5f7adb651fc1c2d50111882e8a143dee4bfdd
Hashir Dilmurad Zai <dzhashirdilmuradzai12@gmail.com>
2026-06-12 15:29:14 +0500

    JS Assignment 1

---
+++ script.js
@@ -0,0 +1,5 @@
+alert("Welcome to JS Let's add 2 numbers");
+let firstNumber = Number(prompt("Enter first number:"));
+let secondNumber = Number(prompt("Enter second number:"));
+let totalNumber = (firstNumber + secondNumber);
+alert(totalNumber);
